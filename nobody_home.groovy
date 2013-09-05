9de49d7a04d27ec1958ee5cc45bd26cfdf4fd29e
imbrianj <bjohnsonwsu@gmail.com>
2013-09-05 13:33:22 -0700

    Tidying up

--- nobody_home.groovy
+++ nobody_home.groovy
@@ -12,136 +12,144 @@
  */
 
 preferences {
-	section("When all of these people leave home") {
-		input "people", "capability.presenceSensor", multiple: true
-	}
+  section("When all of these people leave home") {
+    input "people", "capability.presenceSensor", multiple: true
+  }
 
-	section("Change to this mode to...") {
-		input "newAwayMode", "mode", title: "Everyone is away"
-        input "newHomeMode", "mode", title: "At least one person home"
-	}
+  section("Change to this mode to...") {
+    input "newAwayMode", "mode", title: "Everyone is away"
+    input "newHomeMode", "mode", title: "At least one person home"
+  }
 
-	section("False alarm threshold (defaults to 10 min)") {
-		input "falseAlarmThreshold", "decimal", title: "Number of minutes", required: false
-	}
+  section("False alarm threshold (defaults to 10 min)") {
+    input "falseAlarmThreshold", "decimal", title: "Number of minutes", required: false
+  }
 
-	section( "Notifications" ) {
-		input "sendPushMessage", "enum", title: "Send a push notification?", metadata:[values:["Yes","No"]], required:false
-	}
+  section( "Notifications" ) {
+    input "sendPushMessage", "enum", title: "Send a push notification?", metadata:[values:["Yes","No"]], required:false
+  }
 
-	section("Automatically turn off these switches when away...") {
-		input "switches", "capability.switch", multiple: true
-	}
+  section("Automatically turn off these switches when away...") {
+    input "switches", "capability.switch", multiple: true
+  }
 
-    section("Automatically turn off these thermostats when away... ") {
-		input "thermostats", "capability.thermostat", multiple: true
-	}
+  section("Automatically turn off these thermostats when away...") {
+    input "thermostats", "capability.thermostat", multiple: true
+  }
 }
 
 def installed() {
-	log.debug "Installed with settings: ${settings}"
-	log.debug "Current mode = ${location.mode}, people = ${people.collect{it.label + ': ' + it.currentPresence}}"
-	subscribe(people, "presence", presence)
+  log.debug "Installed with settings: ${settings}"
+  log.debug "Current mode = ${location.mode}, people = ${people.collect{it.label + ': ' + it.currentPresence}}"
+  subscribe(people, "presence", presence)
 }
 
 def updated() {
-	log.debug "Updated with settings: ${settings}"
-	log.debug "Current mode = ${location.mode}, people = ${people.collect{it.label + ': ' + it.currentPresence}}"
-	unsubscribe()
-	subscribe(people, "presence", presence)
+  log.debug "Updated with settings: ${settings}"
+  log.debug "Current mode = ${location.mode}, people = ${people.collect{it.label + ': ' + it.currentPresence}}"
+  unsubscribe()
+  subscribe(people, "presence", presence)
 }
 
 def presence(evt) {
-	log.debug "evt.name: $evt.value"
-	if (evt.value == "not present") {
-		if (location.mode != newAwayMode) {
-			log.debug "checking if everyone is away"
-			if (everyoneIsAway()) {
-				log.debug "starting ${newAwayMode} sequence"
-				def delay = (falseAlarmThreshold != null && falseAlarmThreshold != "") ? falseAlarmThreshold * 60 : 10 * 60 
-				runIn(delay, "setAway")
-			}
-		}
+  log.debug("evt.name: ${evt.value}")
+  if (evt.value == "not present") {
+    if (location.mode != newAwayMode) {
+      log.debug("checking if everyone is away")
 
-		else {
-			log.debug "mode is the same, not evaluating"
-		}
+      if (everyoneIsAway()) {
+        log.info("Starting ${newAwayMode} sequence")
+        def delay = (falseAlarmThreshold != null && falseAlarmThreshold != "") ? falseAlarmThreshold * 60 : 10 * 60 
+        runIn(delay, "setAway")
+      }
+    }
 
-		unschedule("setHome")
-	}
+    else {
+      log.debug("mode is the same, not evaluating")
+    }
 
-	else {
-    	if (location.mode != newHomeMode) {
-			log.debug "checking if anyone is away"
-            if (anyoneIsHome()) {
-				log.debug "starting ${newHomeMode} sequence"
-				setHome()
-			}
-        }
+    unschedule("setHome")
+  }
 
-        else {
-			log.debug "mode is the same, not evaluating"
-		}
+  else {
+    if (location.mode != newHomeMode) {
+      log.debug("checking if anyone is away")
 
-		unschedule("setAway")
-	}
+      if (anyoneIsHome()) {
+        log.info("starting ${newHomeMode} sequence")
+        setHome()
+      }
+    }
+
+    else {
+      log.debug("mode is the same, not evaluating")
+    }
+
+    unschedule("setAway")
+  }
 }
 
 def setAway() {
-	// TODO -- uncomment when app label is available
-	//def message = "${app.label} changed your mode to '${newAwayMode}' because everyone left home"
-	def message = "SmartThings changed your mode to '${newAwayMode}' because everyone left home"
-	log.info message
-	send(message)
-	setLocationMode(newAwayMode)
-	switches?.off()
-  	thermostats?.off()
+  def message = "${app.label} changed your mode to '${newAwayMode}' because everyone left home"
+  log.info message
+  send(message)
+  setLocationMode(newAwayMode)
+  switches?.off()
+// TODO: Figure out how to query if method exists
+  if(thermostats?.away) {
+    log.info("Setting thermostats to away")
     thermostats?.away()
-	unschedule("setAway") // Temporary work-around to scheduling bug
+  }
+  else {
+    log.info("Turning off thermostats")
+    thermostats?.off()
+  }
+  unschedule("setAway") // Temporary work-around to scheduling bug
 }
 
 def setHome() {
-	// TODO -- uncomment when app label is available
-	//def message = "${app.label} changed your mode to '${newHomeMode}' because somebody came home"
-	def message = "SmartThings changed your mode to '${newHomeMode}' because somebody came home"
-	log.info message
-	send(message)
-	setLocationMode(newHomeMode)
+  def message = "${app.label} changed your mode to '${newHomeMode}' because somebody came home"
+  log.info message
+  send(message)
+  setLocationMode(newHomeMode)
+// TODO: Figure out how to query if method exists
+  if(thermostats?.present) {
     thermostats?.present()
-	unschedule("setHome") // Temporary work-around to scheduling bug
+  }
+  unschedule("setHome") // Temporary work-around to scheduling bug
 }
 
 private everyoneIsAway() {
-	def result = true
-	for (person in people) {
-		if (person.currentPresence == "present") {
-			result = false
-			break
-		}
-	}
+  def result = true
+  for (person in people) {
+    if (person.currentPresence == "present") {
+      result = false
+      break
+    }
+  }
 
-	log.debug "everyoneIsAway: $result"
-	return result
+  log.debug("everyoneIsAway: ${result}")
+  return result
 }
 
 private anyoneIsHome() {
-	def result = false
-	for (person in people) {
-		if (person.currentPresence == "present") {
-			result = true
-			break
-		}
-	}
+  def result = false
+  for (person in people) {
+    if (person.currentPresence == "present") {
+      result = true
+      break
+    }
+  }
 
-	log.debug "anyoneIsHome: $result"
-	return result
+  log.debug("anyoneIsHome: ${result}")
+  return result
 }
 
 private send(msg) {
-	if ( sendPushMessage != "No" ) {
-		log.debug( "sending push message" )
-		sendPush( msg )
-	}
+  if (sendPushMessage != "No") {
+    log.debug("sending push message")
+    sendPush(msg)
+  }
 
-	log.debug msg
+  log.debug(msg)
 }
